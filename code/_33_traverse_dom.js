778b7fbdcfb3befbe93c83f0048e7321843025d8
riaz4519 <riaz.i3216@gmail.com>
2019-07-02 01:43:28 +0600

    after addin 34

--- code/_33_traverse_dom.js
+++ code/_33_traverse_dom.js
@@ -3,3 +3,7 @@ var contents = document.getElementsByClassName('content');
 var tagName = contents[1].getElementsByTagName('p');
 
 console.log(tagName[0].innerHTML);
+
+var getBody = document.getElementsByTagName("body");
+
+console.log(getBody[0].innerHTML);
