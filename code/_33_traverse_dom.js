125d1ba89e899a9df41a09a070a0fcb24d28566f
riaz4519 <riaz.i3216@gmail.com>
2019-07-03 00:55:24 +0600

    after 38

--- code/_33_traverse_dom.js
+++ code/_33_traverse_dom.js
@@ -7,3 +7,30 @@ console.log(tagName[0].innerHTML);
 var getBody = document.getElementsByTagName("body");
 
 console.log(getBody[0].innerHTML);
+
+// getting attribute items
+
+    var myAtt = document.getElementById("test")
+    
+    console.log(myAtt.getAttribute('class'));
+
+//set attribute
+
+    myAtt.setAttribute('class','pie');
+
+    myAtt.setAttribute('title','hello');
+
+//class name getting 
+
+    myAtt.className = 'ninja';
+
+//getting the href
+    console.log(myAtt.href);
+
+//getting the style
+
+console.log(myAtt.style)
+
+myAtt.classList.add('dadas');
+
+    
